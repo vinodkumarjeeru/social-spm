9df0d936febb08ea8ed05a842c3edbc16e0a0e84
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-04 10:21:42 +0000

    tables created for parliment and Assembly conctituancy

--- trunk/socialapp/database/mysql/schema.sql
+++ trunk/socialapp/database/mysql/schema.sql
@@ -14,3 +14,32 @@ create table spm_district (
    PRIMARY KEY (dis_id)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;
 
+
+
+create table spm_assembly(
+	asmc_id 			   bigint(10) not null auto_increment,
+	asmc_guid 			   varchar(255) not null,
+	asmc_constituancyid    bigint(10) not null,
+	asmc_constituancyname  varchar(255) not null,
+    asmc_created_date 	   datetime not null,
+    asmc_created_by 	   bigint(10),
+    asmc_modified_date 	   datetime not null,
+    asmc_modified_by       bigint(10),
+    asmc_active 		   bigint(10) not null,
+    PRIMARY KEY (asmc_id)
+)ENGINE = InnoDB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;
+
+
+create table spm_parliment(
+
+	pmc_id 				  bigint(10) not null auto_increment,
+	pmc_guid 			  varchar(255) not null,
+	pmc_constituancyid    bigint(10) not null,
+	pmc_constituancyname  varchar(255) not null,
+    pmc_created_date 	  datetime not null,
+    pmc_created_by 		  bigint(10),
+    pmc_modified_date 	  datetime not null,
+    pmc_modified_by 	  bigint(10),
+    pmc_active 			  bigint(10) not null,
+    PRIMARY KEY (pmc_id)
+)ENGINE = InnoDB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;
