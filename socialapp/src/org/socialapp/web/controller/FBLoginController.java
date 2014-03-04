4a53ae6d3c0b1f26f6d87f7824b986ca46d0b561
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-04 09:17:28 +0000

    Changed FBLoginController.java

--- socialapp/src/org/socialapp/web/controller/FBLoginController.java
+++ socialapp/src/org/socialapp/web/controller/FBLoginController.java
@@ -10,9 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.FacebookApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
 import org.scribe.model.Token;
+import org.scribe.model.Verb;
 import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
 
@@ -26,13 +30,13 @@ public class FBLoginController extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(FBLoginController.class);
 
 	private static final String PROTECTED_RESOURCE_URL = "https://graph.facebook.com/me";
 	private static final String SCOPE = "email,offline_access,user_status,publish_actions,publish_stream,read_friendlists";
 	private static final Token EMPTY_TOKEN = null;
 	private OAuthService service;
 	private Token accessToken = null;
-	private String fbUserId = null;
 	private Verifier verifier = null;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -50,6 +54,26 @@ public class FBLoginController extends HttpServlet {
 
 		String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
 
+		LOG.debug("\n" + authorizationUrl + "&scope=" + SCOPE);
+		String url = authorizationUrl + "&scope=" + SCOPE;
+
+		response.sendRedirect(url);
+
+		if (request.getParameter("code") != null) {
+			verifier = new Verifier(request.getParameter("code"));
+
+			accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+
+			OAuthRequest oAuthRequest = new OAuthRequest(Verb.GET,
+					PROTECTED_RESOURCE_URL);
+			service.signRequest(accessToken, oAuthRequest);
+			Response oAuthResponse = oAuthRequest.send();
+
+			LOG.debug("\n" + oAuthResponse.getBody());
+		}
+
 	}
+
+	
 
 }
