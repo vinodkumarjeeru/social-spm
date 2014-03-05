ebc1ec0de167ee29cbd89967a6f83f17179369ca
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 00:55:37 +0000

    Added GenericDAO to the App

---
+++ socialapp/src/org/socialapp/dao/DistrictDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.socialapp.dao;
+
+import java.math.BigDecimal;
+
+import org.socialapp.domain.sub.District;
+
+/**
+ * @author Sudarsan
+ *
+ */
+public interface DistrictDAO extends GenericDAO<District, BigDecimal>{
+
+}
