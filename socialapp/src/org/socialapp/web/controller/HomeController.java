0c5bc3200f3a25a53f5156deab98fcb618e3a994
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 02:30:28 +0000

    Changed the implementation to RootServlet

--- socialapp/src/org/socialapp/web/controller/HomeController.java
+++ socialapp/src/org/socialapp/web/controller/HomeController.java
@@ -3,18 +3,19 @@ package org.socialapp.web.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeController extends HttpServlet {
+import org.socialapp.web.utils.RootServlet;
+
+public class HomeController extends RootServlet {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	public void socialService(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.sendRedirect("userHome.jsp");
