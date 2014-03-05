d64919cc00d7513a275ecffe91dacb12ed358533
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 04:29:19 +0000

    Modified some fields names in District table also add a coloumn dis_map as mediumblob datatype.

--- trunk/socialapp/database/mysql/schema.sql
+++ trunk/socialapp/database/mysql/schema.sql
@@ -6,11 +6,12 @@ create table spm_district (
    dis_name 		    varchar(255),
    dis_capital 		    varchar(255),
    dis_state 		    varchar(255),   
-   dis_created_date 	datetime not null,
+   dis_created_on 	    datetime not null,
    dis_created_by 		bigint(10),
-   dis_modified_date 	datetime not null,
+   dis_modified_on 	    datetime not null,
    dis_modified_by 		bigint(10),
    dis_active 			bigint(10) not null,
+   dis_map 				mediumblob,
    PRIMARY KEY (dis_id)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;
 
@@ -21,9 +22,9 @@ create table spm_assembly(
 	asmc_guid 			   varchar(255) not null,
 	asmc_constituancyid    bigint(10) not null,
 	asmc_constituancyname  varchar(255) not null,
-    asmc_created_date 	   datetime not null,
+    asmc_created_on 	   datetime not null,
     asmc_created_by 	   bigint(10),
-    asmc_modified_date 	   datetime not null,
+    asmc_modified_on 	   datetime not null,
     asmc_modified_by       bigint(10),
     asmc_active 		   bigint(10) not null,
     PRIMARY KEY (asmc_id)
@@ -31,14 +32,13 @@ create table spm_assembly(
 
 
 create table spm_parliment(
-
 	pmc_id 				  bigint(10) not null auto_increment,
 	pmc_guid 			  varchar(255) not null,
 	pmc_constituancyid    bigint(10) not null,
 	pmc_constituancyname  varchar(255) not null,
-    pmc_created_date 	  datetime not null,
+    pmc_created_on 	  datetime not null,
     pmc_created_by 		  bigint(10),
-    pmc_modified_date 	  datetime not null,
+    pmc_modified_on 	  datetime not null,
     pmc_modified_by 	  bigint(10),
     pmc_active 			  bigint(10) not null,
     PRIMARY KEY (pmc_id)
