fc357e13d35d00a086cc610bde047feab50d7e19
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 15:34:33 +0000

    Changed FBLoginController.java

--- trunk/socialapp/src/org/socialapp/web/controller/FBLoginController.java
+++ trunk/socialapp/src/org/socialapp/web/controller/FBLoginController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.FacebookApi;
 import org.scribe.model.OAuthRequest;
@@ -32,15 +34,12 @@ public class FBLoginController extends RootServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = Logger.getLogger(FBLoginController.class);
 
-	private static final String PROTECTED_RESOURCE_URL = "https://graph.facebook.com/me";
 	private static final String SCOPE = "email,offline_access,user_status,publish_actions,publish_stream,read_friendlists";
 	private static final Token EMPTY_TOKEN = null;
 	private OAuthService service;
-	private Token accessToken = null;
-	private Verifier verifier = null;
 
-	public void socialService(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void socialService(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
 		String apiKey = this.getServletConfig()
 				.getInitParameter("app.config.oauth.facebook.apikey").trim();
@@ -54,26 +53,32 @@ public class FBLoginController extends RootServlet {
 
 		String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
 
-		LOG.debug("\n" + authorizationUrl + "&scope=" + SCOPE);
 		String url = authorizationUrl + "&scope=" + SCOPE;
 
 		response.sendRedirect(url);
 
-		if (request.getParameter("code") != null) {
-			verifier = new Verifier(request.getParameter("code"));
-
-			accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
-
-			OAuthRequest oAuthRequest = new OAuthRequest(Verb.GET,
-					PROTECTED_RESOURCE_URL);
-			service.signRequest(accessToken, oAuthRequest);
-			Response oAuthResponse = oAuthRequest.send();
-
-			LOG.debug("\n" + oAuthResponse.getBody());
-		}
+		// if (request.getParameter("code") != null) {
+		// verifier = new Verifier(request.getParameter("code"));
+		//
+		// accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+		//
+		// OAuthRequest oAuthRequest = new OAuthRequest(Verb.GET,
+		// PROTECTED_RESOURCE_URL);
+		// service.signRequest(accessToken, oAuthRequest);
+		// Response oAuthResponse = oAuthRequest.send();
+		//
+		// JSONObject jsonObject = null;
+		//
+		// try {
+		// jsonObject = new JSONObject(oAuthResponse.getBody());
+		// } catch (JSONException e) {
+		// LOG.warn("", e);
+		// }
+		//
+		// LOG.debug("\n======" + jsonObject.toString());
+		//
+		// }
 
 	}
-
-	
 
 }
