45c45b3cc10aabc214a1d35b54e67ca82dfc24b4
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 15:33:34 +0000

    Added Test

--- trunk/socialapp/src/org/socialapp/tests/Test.java
+++ trunk/socialapp/src/org/socialapp/tests/Test.java
@@ -1,27 +1,63 @@
 package org.socialapp.tests;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
 import org.socialapp.domain.sub.District;
 import org.socialapp.service.DistrictService;
 import org.socialapp.service.impl.DistrictServiceImpl;
 
 public class Test {
 
+	private static final Logger LOG = Logger.getLogger(Test.class);
+
 	public static void main(String[] args) {
 
 		DistrictService districtService = new DistrictServiceImpl();
+		District district = districtService.findBy("Krishna");
 
-		District district = new District();
-		district.setDistrictId(1L);
-		district.setDistrictName("Krishna");
-		district.setDistrictCapital("MTM");
-		district.setState("AP");
-		district.setCreatedOn(new java.util.Date());
-		district.setCreatedBy(1L);
-		district.setModifiedOn(new java.util.Date());
-		district.setModifiedBy(1L);
-		district.setActive(1L);
+		System.out.println(district.getDistrictName() + "\t"
+				+ district.getDistrictCapital());
+		// System.out.println(districtService.getAll());
 
-		districtService.create(district);
+		// District district = new District();
+		// district.setId(1L);
+		// district.setDistrictId(1L);
+		// district.setDistrictName("Krishna");
+		// district.setDistrictCapital("MTM");
+		// district.setState("AP");
+		// district.setCreatedOn(new java.util.Date());
+		// district.setCreatedBy(1L);
+		// district.setModifiedOn(new java.util.Date());
+		// district.setModifiedBy(1L);
+		// district.setActive(1L);
+		//
+		// InputStream inputStream = null;
+		// try {
+		// inputStream = new FileInputStream(new File("D:/Tulips.jpg"));
+		// byte[] bytes = new byte[inputStream.available()];
+		// inputStream.read(bytes);
+		// district.setDistrictMap(bytes);
+		//
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// } finally {
+		// if (inputStream != null) {
+		// try {
+		// inputStream.close();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// }
+		//
+		// districtService.create(district);
 
 	}
 
