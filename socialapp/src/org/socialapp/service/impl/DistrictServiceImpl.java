25ff696f44c6e2a3808d9621fd955344fc176d9e
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-05 15:33:07 +0000

    Changed DistrictServiceImpl.java

--- socialapp/src/org/socialapp/service/impl/DistrictServiceImpl.java
+++ socialapp/src/org/socialapp/service/impl/DistrictServiceImpl.java
@@ -1,7 +1,10 @@
 package org.socialapp.service.impl;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.socialapp.dao.DistrictDAO;
 import org.socialapp.dao.impl.DistrictDAOImpl;
 import org.socialapp.domain.sub.District;
@@ -10,12 +13,13 @@ import org.socialapp.utils.HibernateUtils;
 
 public class DistrictServiceImpl implements DistrictService {
 
-	private static final Logger LOG = Logger.getLogger(DistrictServiceImpl.class);
-	
+	private static final Logger LOG = Logger
+			.getLogger(DistrictServiceImpl.class);
+
 	private DistrictDAO districtDAO = new DistrictDAOImpl();
 
 	@Override
-	public void create(District district) {
+	public void createOrUpdate(District district) {
 
 		try {
 			HibernateUtils.beginTransaction();
@@ -25,6 +29,41 @@ public class DistrictServiceImpl implements DistrictService {
 			LOG.warn("", exception);
 		}
 
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<District> getAll() {
+		List<District> list = null;
+		try {
+			HibernateUtils.beginTransaction();
+			list = districtDAO.getAll(District.class);
+			HibernateUtils.commitTransaction();
+		} catch (Exception exception) {
+			LOG.warn("", exception);
+		}
+
+		return list;
+	}
+
+	@Override
+	public District findBy(String districtName) {
+		District district = null;
+		try {
+			HibernateUtils.beginTransaction();
+			Query query = HibernateUtils
+					.currentSession()
+					.createQuery(
+							"from District district where district.districtName=:districtName")
+					.setParameter("districtName", districtName);
+			district = districtDAO.findOne(query);
+			HibernateUtils.closeSession();
+
+		} catch (Exception exception) {
+			LOG.warn("", exception);
+
+		}
+		return district;
 	}
 
 }
