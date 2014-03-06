cd724099936168d8ae957347ae5f336eb699d57a
chvijay.india@gmail.com <chvijay.india@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-06 03:23:54 +0000

    Adding XUserService.java

---
+++ trunk/socialapp/src/org/socialapp/service/XUserService.java
@@ -0,0 +1,18 @@
+package org.socialapp.service;
+
+import java.util.List;
+
+import org.socialapp.domain.sub.XUser;
+
+/**
+ * @author vijay
+ * 
+ */
+public interface XUserService {
+	void createOrUpdate(XUser xUser);
+
+	List<XUser> getAll();
+
+	XUser findBy(String userName);
+
+}
