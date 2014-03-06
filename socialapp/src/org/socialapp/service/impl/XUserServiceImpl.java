f54d3e9faf74025ccb73627734c30953e1f3631e
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-06 09:15:39 +0000

    Changed XUserServiceImpl.java

--- socialapp/src/org/socialapp/service/impl/XUserServiceImpl.java
+++ socialapp/src/org/socialapp/service/impl/XUserServiceImpl.java
@@ -2,13 +2,28 @@ package org.socialapp.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.socialapp.dao.XUserDAO;
+import org.socialapp.dao.impl.XUserDAOImpl;
 import org.socialapp.domain.sub.XUser;
 import org.socialapp.service.XUserService;
+import org.socialapp.utils.HibernateUtils;
 
 public class XUserServiceImpl implements XUserService {
+	private static final Logger LOG = Logger.getLogger(XUserServiceImpl.class);
+	private XUserDAO xUserDAO = new XUserDAOImpl();
 
 	@Override
 	public void createOrUpdate(XUser xUser) {
+
+		try {
+			HibernateUtils.beginTransaction();
+			xUserDAO.save(xUser);
+			HibernateUtils.commitTransaction();
+		} catch (Exception exception) {
+			LOG.warn("", exception);
+		}
 
 	}
 
@@ -20,6 +35,26 @@ public class XUserServiceImpl implements XUserService {
 	@Override
 	public XUser findBy(String userName) {
 		return null;
+	}
+
+	@Override
+	public XUser authenticate(String email, String password) {
+		XUser user = null;
+		try {
+			HibernateUtils.beginTransaction();
+			Query query = HibernateUtils
+					.currentSession()
+					.createQuery(
+							"from XUser xUser where xUser.email=:email and xUser.password=:password")
+					.setParameter("email", email)
+					.setParameter("password", password);
+			user = xUserDAO.findOne(query);
+			HibernateUtils.commitTransaction();
+
+		} catch (Exception exception) {
+			LOG.debug("", exception);
+		}
+		return user;
 	}
 
 }
