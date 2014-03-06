6f4b36372a0d0bfcba727215d39260e9268a01fd
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-06 09:34:53 +0000

    Added complete Functionality to XUserController.java(Servlet)

--- socialapp/src/org/socialapp/web/controller/XUserController.java
+++ socialapp/src/org/socialapp/web/controller/XUserController.java
@@ -13,6 +13,8 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.log4j.Logger;
 import org.socialapp.domain.sub.XUser;
+import org.socialapp.service.XUserService;
+import org.socialapp.service.impl.XUserServiceImpl;
 import org.socialapp.web.utils.RootServlet;
 
 /**
@@ -30,32 +32,45 @@ public class XUserController extends RootServlet {
 		try {
 			@SuppressWarnings("unchecked")
 			List<FileItem> list = upload.parseRequest(request);
-			FileItem email = list.get(0);
-			String mailId = email.getString().trim();
-			FileItem username = list.get(1);
-			String name = username.getString().trim();
-			FileItem password = list.get(2);
-			String password1 = password.getString().trim();
-			FileItem securityQuestion = list.get(3);
-			String secQuestion = securityQuestion.getString().trim();
-			FileItem answer = list.get(4);
-			String answer1 = answer.getString().trim();
-			FileItem mobile = list.get(5);
-			String mobileNumber = mobile.getString().trim();
-			FileItem profilePicture = list.get(6);
-			LOG.debug(mailId);
-			LOG.debug(name);
-			LOG.debug(password1);
-			LOG.debug(secQuestion);
-			LOG.debug(answer1);
+			FileItem firstName = list.get(0);
+			String name1 = firstName.getString();
+			FileItem lastName = list.get(1);
+			String name2 = lastName.getString();
+			FileItem email = list.get(2);
+			String mail = email.getString();
+			FileItem password = list.get(3);
+			String pwd = password.getString();
+			FileItem confirmPassword = list.get(4);
+			String cpwd = confirmPassword.getString();
+			FileItem districtName = list.get(5);
+			String dName = districtName.getString();
+			FileItem mobile = list.get(6);
+			String mobileNumber = mobile.getString();
+			FileItem securityQuestion = list.get(7);
+			String secQuestion = securityQuestion.getString();
+			FileItem answer = list.get(8);
+			String ans = answer.getString();
+			FileItem profilePicture = list.get(9);
+
+			LOG.debug(name1);
+			LOG.debug(name2);
+			LOG.debug(mail);
+			LOG.debug(pwd);
+			LOG.debug(cpwd);
+			LOG.debug(dName);
 			LOG.debug(mobileNumber);
-			LOG.debug(profilePicture.get().length);
-			
+			LOG.debug(secQuestion);
+			LOG.debug(ans);
+			//LOG.debug(profilePicture.get().length);
+
 			XUser xUser = new XUser();
-			xUser.setEmail(mailId);			
-			xUser.setPassword(password1);
+			// xUser.setFbId(1L);
+			xUser.setFirstName(name1);
+			xUser.setLastName(name2);
+			xUser.setEmail(mail);
+			xUser.setPassword(pwd);
 			xUser.setSecurityQuestion(secQuestion);
-			xUser.setAnswer(answer1);
+			xUser.setAnswer(ans);
 			xUser.setMobile(mobileNumber);
 			xUser.setProfilePic(profilePicture.get());
 			xUser.setCreatedBy(1L);
@@ -63,6 +78,9 @@ public class XUserController extends RootServlet {
 			xUser.setModifiedBy(1L);
 			xUser.setModifiedOn(new java.util.Date());
 			xUser.setActive(1L);
+			// LOG.debug(xUser);
+			XUserService service = new XUserServiceImpl();
+			service.createOrUpdate(xUser);
 
 		} catch (FileUploadException e) {
 			LOG.debug(e);
