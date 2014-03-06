8f1cde158fe8f0ffa260f7a5b1dd4bffd0a374d2
chvijay.india@gmail.com <chvijay.india@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-06 03:24:39 +0000

    Adding XUserServiceImpl.java

---
+++ trunk/socialapp/src/org/socialapp/service/impl/XUserServiceImpl.java
@@ -0,0 +1,25 @@
+package org.socialapp.service.impl;
+
+import java.util.List;
+
+import org.socialapp.domain.sub.XUser;
+import org.socialapp.service.XUserService;
+
+public class XUserServiceImpl implements XUserService {
+
+	@Override
+	public void createOrUpdate(XUser xUser) {
+
+	}
+
+	@Override
+	public List<XUser> getAll() {
+		return null;
+	}
+
+	@Override
+	public XUser findBy(String userName) {
+		return null;
+	}
+
+}
