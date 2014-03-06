ca005649064f4ca67280c8ff3b4f0c59d03e185b
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-06 09:15:12 +0000

    Changed XUserService

--- trunk/socialapp/src/org/socialapp/service/XUserService.java
+++ trunk/socialapp/src/org/socialapp/service/XUserService.java
@@ -9,10 +9,13 @@ import org.socialapp.domain.sub.XUser;
  * 
  */
 public interface XUserService {
+	
 	void createOrUpdate(XUser xUser);
 
 	List<XUser> getAll();
 
 	XUser findBy(String userName);
+	
+	XUser authenticate(String email, String password);
 
 }
