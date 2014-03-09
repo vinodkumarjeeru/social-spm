a1de208a240310a0d34fbdb1a948c5e8a30f6213
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-09 10:33:35 +0000

    Changed social.js for POSTing SignIn Form

--- trunk/socialapp/WebContent/js/social.js
+++ trunk/socialapp/WebContent/js/social.js
@@ -1,25 +1,12 @@
 
 var SocialApp = {
-    ajaxCall: function(url, type, form) {
-
-        var result = "";
-
-        $.ajax({
-            url: ctx + url,
-            async: false,
-            type: type,
-            data: form.serialize(),
-            success: function(response) {
-                result = response;
-            }
-        });
-        return result;
-    },
     resetSignUpForm: function() {
         $('#email').val('');
-        $('#password').val('');       
+        $('#password').val('');
+        $('#info').hide();
     },
-    signup: function() {
+    signin: function(e) {
+        e.preventDefault();
         var email = $('#email').val();
         var password = $('#password').val();
 
@@ -31,26 +18,29 @@ var SocialApp = {
             $('#lblPassword').addClass('form-group has-error');
             return;
         }
+
         $('#btnSignIn').attr('disabled', 'disabled');
         $('#ajaxLoad').show();
-        setTimeout(function() {
-            $('#ajaxLoad').hide();
-            var ajaxResponse = SocialApp.ajaxCall('/login', 'POST', $('#signInForm'));
-            if(ajaxResponse == 1) {
-                window.location = ctx+'/userHome.jsp';
-            }else {
-                $('#info').show();
-            }
-            
-        }, 1000);
+        $.post(ctx + '/login',
+                $('#signInForm').serialize(),
+                function(response) {
+                   $('#ajaxLoad').hide();
+                    if(response == 1) {
+                        window.location = ctx + '/userHome.jsp';
+                    }else {
+                        $('#info').show();
+                        $('#btnSignIn').removeAttr('disabled');
+                    }
+                    
+                }
+        );
+
+
     },
     initHome: function() {
 
         // App ContextPath
         ctx = $('#ctx').val();
-
-        //Hide Ajax Load
-        $('#ajaxLoad').hide();
 
         // Reset for Form Errors        
         SocialApp.resetSignUpForm;
@@ -58,8 +48,11 @@ var SocialApp = {
         // Hide info Element on SignIn
         $('#info').hide();
 
+        // AJAX Loader Hide on Startup.
+        $('#ajaxLoad').hide();
+        
         // Sign In Action
-        $('#btnSignIn').click(SocialApp.signup);
+        $('#btnSignIn').click(SocialApp.signin);
 
         // Clear Button Action
         $('#btnClear').click(SocialApp.resetSignUpForm);
