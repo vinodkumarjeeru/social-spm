379b800ea1d71c0f2235da838796dda15998c817
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-10 09:58:44 +0000

    Added validation code to UserRegistration(XUserController.java)

--- socialapp/src/org/socialapp/web/controller/XUserController.java
+++ socialapp/src/org/socialapp/web/controller/XUserController.java
@@ -27,6 +27,7 @@ public class XUserController extends RootServlet {
 
 	protected void socialService(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		boolean error = false;
 
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		ServletFileUpload upload = new ServletFileUpload(factory);
@@ -34,24 +35,80 @@ public class XUserController extends RootServlet {
 			@SuppressWarnings("unchecked")
 			List<FileItem> list = upload.parseRequest(request);
 			FileItem firstName = list.get(0);
-			String fName = firstName.getString();
+			String fName = firstName.getString().trim();
 			FileItem lastName = list.get(1);
-			String lName = lastName.getString();
+			String lName = lastName.getString().trim();
 			FileItem email = list.get(2);
-			String mailId = email.getString();
+			String mailId = email.getString().trim();
 			FileItem password = list.get(3);
-			String newPassword = password.getString();
+			String newPassword = password.getString().trim();
 			FileItem confirmPassword = list.get(4);
-			String rePassword = confirmPassword.getString();
+			String rePassword = confirmPassword.getString().trim();
 			FileItem districtName = list.get(5);
-			String district = districtName.getString();
+			String district = districtName.getString().trim();
 			FileItem mobile = list.get(6);
-			String mobileNumber = mobile.getString();
+			String mobileNumber = mobile.getString().trim();
 			FileItem securityQuestion = list.get(7);
-			String secQuestion = securityQuestion.getString();
+			String secQuestion = securityQuestion.getString().trim();
 			FileItem answer = list.get(8);
-			String givenAnswer = answer.getString();
+			String givenAnswer = answer.getString().trim();
 			FileItem profilePicture = list.get(9);
+
+			if (fName == null || fName.length() == 0) {
+				request.setAttribute("firstNameError",
+						"Please Enter Your First Name");
+				error = true;
+			}
+			if (lName == null || lName.length() == 0) {
+				request.setAttribute("lastNameError",
+						"Please Enter Your Last Name");
+				error = true;
+			}
+			if (mailId == null || mailId.length() == 0) {
+				request.setAttribute("mailIdError", "Please Enter Your Mail Id");
+				error = true;
+			}
+			if (newPassword == null || newPassword.length() == 0) {
+				request.setAttribute("newPasswordError",
+						"Please Enter Your Password");
+				error = true;
+			}
+			if (rePassword == null || rePassword.length() == 0) {
+				request.setAttribute("rePasswordError",
+						"Please Re-Enter Your Password");
+				error = true;
+			}
+			if (!newPassword.equals(rePassword)) {
+				request.setAttribute("checkPasswordError",
+						"Passwords Mismatch! Enter Same Passwords");
+				error = true;
+			}
+			if (district == null || district.length() == 0) {
+				request.setAttribute("districtError",
+						"Please Select Your District");
+				error = true;
+			}
+			if (mobileNumber == null || mobileNumber.length() == 0) {
+				request.setAttribute("mobileNumberError",
+						"Please Enter Your Mobile Number");
+				error = true;
+			}
+			if (secQuestion.equalsIgnoreCase("Security Question")) {
+				request.setAttribute("secQuestionError",
+						"Please Choose Your Security Question");
+				error = true;
+			}
+			if (givenAnswer == null || givenAnswer.length() == 0) {
+				request.setAttribute("givenAnswerError",
+						"Please Enter Your Answer");
+				error = true;
+			}
+			if (error) {
+				RequestDispatcher dispatcher = request
+						.getRequestDispatcher("/register.jsp");
+				dispatcher.forward(request, response);
+				return;
+			}
 
 			LOG.debug(fName);
 			LOG.debug(lName);
@@ -63,10 +120,11 @@ public class XUserController extends RootServlet {
 			LOG.debug(secQuestion);
 			LOG.debug(givenAnswer);
 			// LOG.debug(profilePicture.get().length);
-			
-			if(! newPassword.equalsIgnoreCase(rePassword)) {
+
+			if (!newPassword.equalsIgnoreCase(rePassword)) {
 				request.setAttribute("errMsg", "Please Confirm your Password");
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/register.jsp");
+				RequestDispatcher dispatcher = request
+						.getRequestDispatcher("/register.jsp");
 				dispatcher.forward(request, response);
 				return;
 			}
@@ -91,8 +149,7 @@ public class XUserController extends RootServlet {
 			XUserService service = new XUserServiceImpl();
 			service.createOrUpdate(xUser);
 
-			request.setAttribute("msg",
-					"Registration Completed, ");
+			request.setAttribute("msg", "Registration Completed, ");
 
 			RequestDispatcher dispatcher = request
 					.getRequestDispatcher("/register.jsp");
