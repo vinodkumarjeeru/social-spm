e3f10e9a1a85c300da3fb77e450c98b0a876256a
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-10 09:45:08 +0000

    Added new Queries

--- socialapp/src/org/socialapp/queries/SocialAppQueries.java
+++ socialapp/src/org/socialapp/queries/SocialAppQueries.java
@@ -1,6 +1,12 @@
 package org.socialapp.queries;
 
 public interface SocialAppQueries {
+
 	public static final String DISTRICT_FIND_BY_DISTRICTNAME = "from District district where district.districtName=:districtName";
+
 	public static final String XUSER_AUTHENTICATE = "from XUser xUser where xUser.email=:email and xUser.password=:password";
+
+	public static final String XUSER_GET_PASSWORD = "from XUser xUser where xUser.email=:email and xUser.securityQuestion=:securityQuestion and xUser.answer=:answer";
+
+	public static final String XUSER_FIND_BY_EMAIL = "from XUser xUser where xUser.email=:email";
 }
