cd6a5c99ca15a11592abccce3788abf1adc96f56
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-10 08:35:02 +0000

    Added some validation code in ForgotPasswordController.java(Servlet)

--- socialapp/src/org/socialapp/web/controller/ForgotPasswordController.java
+++ socialapp/src/org/socialapp/web/controller/ForgotPasswordController.java
@@ -2,6 +2,7 @@ package org.socialapp.web.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,10 +20,32 @@ public class ForgotPasswordController extends RootServlet {
 
 	protected void socialService(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		boolean error = false;
 		String email = request.getParameter("email").trim();
 		String securityQuestion = request.getParameter("securityQuestion")
 				.trim();
 		String answer = request.getParameter("answer").trim();
+
+		if (email.length() == 0 || email == null) {
+			request.setAttribute("emailError", "Please Enter Email Id");
+			error = true;
+		}
+		if (securityQuestion.equalsIgnoreCase("Security Question")) {
+			request.setAttribute("secQuestionError",
+					"Please Select Security Question");
+			error = true;
+		}
+		if (answer.length() == 0 || answer == null) {
+			request.setAttribute("answerError", "Please Enter answer");
+			error = true;
+		}
+		if (error) {
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("/forgotPassword.jsp");
+			dispatcher.forward(request, response);
+			return;
+		}
+
 		LOG.debug(email);
 		LOG.debug(securityQuestion);
 		LOG.debug(answer);
