f69845a8dedc7a6416182b5c538b324e6ca0fa2f
shashank.bhvajjula@gmail.com <shashank.bhvajjula@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-13 04:34:18 +0000

    filter added

---
+++ trunk/socialapp/src/org/soacialapp/web/security/SecurityFilter.java
@@ -0,0 +1,44 @@
+package org.soacialapp.web.security;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.socialapp.domain.sub.XUser;
+
+/**
+ *
+ * @author Admin
+ */
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String uri = request.getRequestURI();
+        if (uri.equalsIgnoreCase(request.getContextPath() + "/userHome.jsp") || uri.equalsIgnoreCase(request.getContextPath() + "/adminHome.jsp")) {
+            XUser user = (XUser) request.getSession().getAttribute("user");
+            if (user == null) {
+                response.sendRedirect("index.jsp");
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
