0bd9e41c65f9e5077c71daea8bec22330442edf2
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-14 05:35:38 +0000

    Created FBUser Object

---
+++ socialapp/src/org/socialapp/domain/sub/FBUser.java
@@ -0,0 +1,225 @@
+package org.socialapp.domain.sub;
+
+import java.util.UUID;
+
+import org.socialapp.domain.Domain;
+
+public class FBUser extends Domain {
+	private String fbId;
+	private String userName;
+	private String gender;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String district;
+	private String securityQuestion;
+	private String answer;
+	private String mobile;
+	private byte[] profilePic;
+
+	public FBUser() {
+		setGuid(UUID.randomUUID().toString());
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the district
+	 */
+	public String getDistrict() {
+		return district;
+	}
+
+	/**
+	 * @param district
+	 *            the district to set
+	 */
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	/**
+	 * @return the securityQuestion
+	 */
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	/**
+	 * @param securityQuestion
+	 *            the securityQuestion to set
+	 */
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	/**
+	 * @return the answer
+	 */
+	public String getAnswer() {
+		return answer;
+	}
+
+	/**
+	 * @param answer
+	 *            the answer to set
+	 */
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	/**
+	 * @return the mobile
+	 */
+	public String getMobile() {
+		return mobile;
+	}
+
+	/**
+	 * @param mobile
+	 *            the mobile to set
+	 */
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	/**
+	 * @return the profilePic
+	 */
+	public byte[] getProfilePic() {
+		return profilePic;
+	}
+
+	/**
+	 * @param profilePic
+	 *            the profilePic to set
+	 */
+	public void setProfilePic(byte[] profilePic) {
+		this.profilePic = profilePic;
+	}
+
+	/**
+	 * @return the fbId
+	 */
+	public String getFbId() {
+		return fbId;
+	}
+
+	/**
+	 * @param fbId
+	 *            the fbId to set
+	 */
+	public void setFbId(String fbId) {
+		this.fbId = fbId;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender
+	 *            the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FBUser [fbId=");
+		builder.append(fbId);
+		builder.append(", userName=");
+		builder.append(userName);
+		builder.append(", gender=");
+		builder.append(gender);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
