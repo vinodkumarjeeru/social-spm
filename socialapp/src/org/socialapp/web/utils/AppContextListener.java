d493e1156efac3c498ef88af51528418b248e596
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-17 13:35:27 +0000

    Removed Logs

--- socialapp/src/org/socialapp/web/utils/AppContextListener.java
+++ socialapp/src/org/socialapp/web/utils/AppContextListener.java
@@ -17,25 +17,20 @@ public class AppContextListener implements ServletContextListener{
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent ctxEvent) {
-		
+	public void contextInitialized(ServletContextEvent ctxEvent) {		
 		
 		ServletContext ctx = ctxEvent.getServletContext();
 		 String log4jConfig = ctx.getInitParameter("log4j-config");
 	        if(log4jConfig == null){
-	            System.err.println("No log4j-config init param, initializing log4j with BasicConfigurator");
 	            BasicConfigurator.configure();
 	        }else {
 	            String webAppPath = ctx.getRealPath("/");
 	            String log4jProp = webAppPath + log4jConfig;
-	            System.out.println("===============Name ==="+log4jProp);
 	            
 	            File log4jConfigFile = new File(log4jProp);
 	            if (log4jConfigFile.exists()) {
-	                System.out.println("Initializing log4j with: " + log4jProp);
 	                DOMConfigurator.configure(log4jProp);
 	            } else {
-	                System.err.println(log4jProp + " file not found, initializing log4j with BasicConfigurator");
 	                BasicConfigurator.configure();
 	            }
 	        }
