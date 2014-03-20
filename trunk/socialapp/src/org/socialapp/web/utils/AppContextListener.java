9cd820db696841ec977b088cffe1369b1d33a5e0
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-20 09:56:39 +0000

    Changed AppContextListener.java

--- trunk/socialapp/src/org/socialapp/web/utils/AppContextListener.java
+++ trunk/socialapp/src/org/socialapp/web/utils/AppContextListener.java
@@ -9,37 +9,29 @@ import javax.servlet.ServletContextListener;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.xml.DOMConfigurator;
 
-public class AppContextListener implements ServletContextListener{
+public class AppContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		
+
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent ctxEvent) {		
-		
-		
-		
+	public void contextInitialized(ServletContextEvent ctxEvent) {
 		ServletContext ctx = ctxEvent.getServletContext();
-		 String log4jConfig = ctx.getInitParameter("log4j-config");
-	        if(log4jConfig == null){
-	            BasicConfigurator.configure();
-	        }else {
-	            String webAppPath = ctx.getRealPath("/");
-	            String log4jProp = webAppPath + log4jConfig;
-	            
-	            File log4jConfigFile = new File(log4jProp);
-	            
-	            System.out.println("====================="+log4jConfigFile.getAbsolutePath());
-	            if (log4jConfigFile.exists()) {
-	                DOMConfigurator.configure(log4jProp);
-	                
-	            } else {
-	                BasicConfigurator.configure();
-	                
-	            }
-	        }
+		String log4jConfig = ctx.getInitParameter("log4j-config");
+		if (log4jConfig == null) {
+			BasicConfigurator.configure();
+		} else {
+			String webAppPath = ctx.getRealPath("/");
+			String log4jProp = webAppPath + log4jConfig;
+			File log4jConfigFile = new File(log4jProp);			
+			if (log4jConfigFile.exists()) {
+				DOMConfigurator.configure(log4jProp);
+			} else {
+				BasicConfigurator.configure();
+			}
+		}
 	}
 
 }
