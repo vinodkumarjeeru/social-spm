07b14979c4b07050548f727a77ea7b2a2310aeb1
zarviscodes@gmail.com <zarviscodes@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-03-21 09:12:13 +0000

    Changed SecurityFilter.java

--- socialapp/src/org/socialapp/web/security/SecurityFilter.java
+++ socialapp/src/org/socialapp/web/security/SecurityFilter.java
@@ -1,6 +1,7 @@
 package org.socialapp.web.security;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,38 +10,60 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.socialapp.domain.sub.FBUser;
 import org.socialapp.domain.sub.XUser;
 
 /**
- *
+ * 
  * @author Admin
  */
 public class SecurityFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig fc) throws ServletException {
-    }
+	private static final Logger LOG = Logger.getLogger(SecurityFilter.class);
 
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+	@Override
+	public void init(FilterConfig fc) throws ServletException {
+	}
 
-    	
-    	HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
 
-        String uri = request.getRequestURI();
-        if (uri.equalsIgnoreCase(request.getContextPath() + "/userHome.jsp") || uri.equalsIgnoreCase(request.getContextPath() + "/adminHome.jsp")) {
-            XUser user = (XUser) request.getSession().getAttribute("user");
-            if (user == null) {
-                response.sendRedirect("index.jsp");
-                return;
-            }
-        }
-        chain.doFilter(request, response);
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 
-    }
+		String uri = request.getRequestURI();
 
-    @Override
-    public void destroy() {
-    }
+		LOG.debug("====================" + uri);
+
+		if (uri.equalsIgnoreCase(request.getContextPath() + "/userHome.jsp")) {
+			XUser user = (XUser) request.getSession().getAttribute("user");
+
+			if (user == null) {
+				response.sendRedirect("index.jsp");
+				return;
+			}
+		}
+
+		if (uri.equalsIgnoreCase(request.getContextPath() + "/adminHome.jsp")) {
+			String fbUser = (String) request.getSession()
+					.getAttribute("fbUser");
+
+			LOG.debug("===================" + fbUser);
+
+			if (fbUser == null) {
+				LOG.debug("========================in Filter");
+				response.sendRedirect("index.jsp");
+				return;
+			}
+		}
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void destroy() {
+	}
 }
