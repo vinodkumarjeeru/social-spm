19d2acc030dbed6a1de343782ea402bebff352ff
vinod.cm46@gmail.com <vinod.cm46@gmail.com@f3c0232d-2f6e-819e-8b98-ca5a59e03126>
2014-04-10 08:43:15 +0000

    Added DistrictServiceImplTest(Testcase) & XUserServiceImplTest(Testcase)

--- socialapp/src/test/java/org/socialapp/service/impl/XUserServiceImplTest.java
+++ socialapp/src/test/java/org/socialapp/service/impl/XUserServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.socialapp.domain.sub.XUser;
 import org.socialapp.service.XUserService;
 import org.socialapp.test.SocialTestCase;
 
@@ -18,8 +19,25 @@ public class XUserServiceImplTest extends SocialTestCase {
 
 	@Test
 	public void testFindBy() {
-		assertNotNull(xUserService.findBy("zarvis.a@gmail.com"));
+		assertNotNull(xUserService.findBy("aaa@gmail.com"));
+	}
 
+	@Test
+	public void testGetAll() {
+		assertNull(xUserService.getAll());
+	}
+
+	@Test
+	public void testAuthenticate() {
+		assertNotNull(xUserService.authenticate("aaa@gmail.com", "aaa"));
+	}
+
+	@Test
+	public void testGetPassword() {
+		String password = xUserService.getPassword("aaa@gmail.com",
+				"What was your dream job as a child?", "Software Engineer");
+		assertEquals(password, xUserService.getPassword("aaa@gmail.com",
+				"What was your dream job as a child?", "Software Engineer"));
 	}
 
 }
